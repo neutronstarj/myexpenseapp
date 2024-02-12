c29c29883cf8fc779e14ac319db01aced6134474
Iris He <jiale14@ualberta.ca>
2024-02-12 13:08:12 -0700

    backend

---
+++ backend/app.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const cors = require('cors');
+const { db } = require('./db/db');
+const {readdirSync} = require('fs')
+const app = express()
+
+require('dotenv').config()
+const PORT = process.env.PORT
+const corsOptions ={
+    origin:'*', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200,
+ }
+ 
+app.use(cors(corsOptions))
+
+
+
+//middlewares
+app.use(express.json())
+
+
+//routes
+readdirSync('./routes').map((route) => app.use('/api/v1', require('./routes/' + route)))
+
+const server = () => {
+    db()
+    app.listen(PORT, () => {
+        console.log('listening to port:', PORT)
+    })
+}
+
+server()
